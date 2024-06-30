e813aa2a4516f3060c22ba01570dbd84e6db3870
dhvanil-7 <dkprajapati46@gmail.com>
2024-06-30 18:47:53 -0400

    feat: add subscriber controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,16 +9,58 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(503, "Please provide a valid channel-id.")
+    }
+
+    const isChannelSubscribed = await Subscription.findOne({
+            channel: channelId,
+            subscriber: req.user?._id
+    })
+
+    let toggleSubscription;
+    if(isChannelSubscribed){
+        toggleSubscription = await Subscription.findByIdAndDelete(isChannelSubscribed._id, {new: true})
+        console.log(toggleSubscription)
+    }
+    else {
+       toggleSubscription = await Subscription.create({
+            subscriber: req.user?._id,
+            channel: channelId
+        })
+    }
+
+    const isSubscribed = await Subscription.findById(toggleSubscription._id)
+
+    return res.status(200).json(new ApiResponse(200, toggleSubscription,
+        isSubscribed ? "Channel is subscribed" : "Channel is unsubscribed"))  
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    console.log(channelId)
+    const all_subscribers = await Subscription.find({
+        channel: channelId
+    })
+
+    return res.status(200).json(new ApiResponse(200, all_subscribers, "Subscribers fetched successfully."))
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if(!subscriberId){
+        throw new ApiError(402, "Please provide subscriber id.")
+    }
+
+    const subscribedChannels = await Subscription.find({
+            subscriber: subscriberId
+        })
+
+    return res.status(200).json(new ApiResponse(200, subscribedChannels, "Subscribed channels are fetched successfully."))
 })
 
 export {
